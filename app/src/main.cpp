37be2fbfff029cef2e8fce24a4f2568e1b357a03
Alejandro Ramirez <alexrmz2394@gmail.com>
2026-02-04 21:41:54 -0600

    Small adjustments for folders/files

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Electronics Inventory - hello, new code here..." << std::endl;
+	std::cout << "Electronics Inventory - hello" << std::endl;
 	return 0;
 }
