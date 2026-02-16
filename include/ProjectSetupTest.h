20e527809a94bc28bcee7086608d7992c75af363
elmfrain <elmfer10@gmail.com>
2026-02-16 13:46:08 -0600

    added project setup test files to test project structure

---
+++ include/ProjectSetupTest.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+ * Note:
+ *
+ * This file is only intended to test project setup. This file MUST be removed
+ * when development of the application begins.
+ */
+
+int RunTestQtApp(int argc, char** argv);
