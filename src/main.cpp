509190b8ffe80d1732e4034d99c095cde9e636e7
elmfrain <elmfer10@gmail.com>
2026-02-16 02:52:20 -0600

    Create source directory

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <QApplication>
+#include <QLabel>
+
+int main(int argCount, char** argValues) {
+	QApplication app(argCount, argValues);
+	QLabel* label = new QLabel("Yo wassup, welcome to Electronics Components Invetory Manager");
+
+	label->setWindowTitle("Electronics Components Inventory Manager");
+	label->resize(600, 400);
+	label->show();
+
+	return app.exec();
+}
