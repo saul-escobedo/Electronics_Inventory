6d3732d1c77804405b04ada8bbca56c596e45c13
elmfrain <elmfer10@gmail.com>
2026-02-16 22:23:31 -0600

    make main.cpp run ECIM app implmentation

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,16 +1,5 @@
-#include <iostream>
-#include <QApplication>
-#include <QLabel>
-#include "example_folder1/dummy11.h"
+#include "Application.hpp"
 
 int main(int argCount, char** argValues) {
-	QApplication app(argCount, argValues);
-	QLabel* label = new QLabel("Yo wassup, welcome to Electronics Components Invetory Manager");
-
-	label->setWindowTitle("Electronics Components Inventory Manager");
-	label->resize(600, 400);
-	label->show();
-
-	dummy11(); // just to test that the function is callable from main.cpp, and that the header is correctly included and linked
-	return app.exec();
+	return execECIMApplication(argCount, argValues);
 }
