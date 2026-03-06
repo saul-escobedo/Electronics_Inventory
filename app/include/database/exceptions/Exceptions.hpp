4d27054aace0d9605a1db861d19d37121903b60c
elmfrain <elmfer10@gmail.com>
2026-03-06 02:30:43 -0600

    Added UninitializedDBException

---
+++ app/include/database/exceptions/Exceptions.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "DatabaseException.hpp"
+#include "UninitializedDatabaseException.hpp"
---
+++ app/include/database/exceptions/UninitializedDatabaseException.hpp
@@ -0,0 +1,24 @@
+#include "DatabaseException.hpp"
+
+#include <cstdio>
+
+namespace ecim {
+    class UninitializedDatabaseException : public DatabaseException {
+    private:
+        std::string _formatMessage(const std::string& backendName) {
+            char message[512];
+
+            snprintf(
+                message,
+                sizeof(message),
+                "An accessor function in an uninitialized %s database was used.",
+                backendName.c_str()
+            );
+
+            return message;
+        }
+    public:
+        UninitializedDatabaseException(std::string backendName)
+        : DatabaseException(_formatMessage(backendName)) {}
+    };
+}
