053c56b99127fd2f23854c33d4a54ceb9c83ebf8
elmfrain <elmfer10@gmail.com>
2026-05-01 14:16:33 -0500

    Changed program version to v0.1.0 and use cmake 3.20

--- app/cmake/EmbedIcons.cmake
+++ app/cmake/EmbedIcons.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.10)
+cmake_minimum_required(VERSION 3.20)
 
 if(WIN32)
     target_sources(${PROJECT_NAME} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/../assets/icons/icon.rc")
